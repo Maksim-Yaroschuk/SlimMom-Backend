d668adb1909aaf6ebb495385fad791bf40d46f5b
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-12-16 18:43:44 +0200

    add variable for PORT

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const { HOST_DB } = process.env;
+const { HOST_DB, PORT } = process.env;
 
 const main = async () => {
   try {
     await mongoose.connect(HOST_DB);
     console.log("Database connection successful");
-    app.listen(3001, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3001");
     });
   } catch (error) {
