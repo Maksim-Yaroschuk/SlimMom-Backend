6f4e223e4cc18daa405f826cb531a3f3cd90dc33
Haik <55463651+salampego@users.noreply.github.com>
2022-12-22 02:58:45 +0200

    оптимізував db з'їденої їжі

--- controllers/myProducts/isFutureDate.js
+++
@@ -1,8 +0,0 @@
-const isFutureDate = (date) => {
-  const inputDay = new Date(date).setHours(0, 0, 0, 0);
-  const today = new Date().setHours(0, 0, 0, 0);
-
-  return inputDay > today;
-};
-
-module.exports = isFutureDate;
