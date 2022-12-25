0b3edbf2225b569a4ff1e7b07be1363f1a9bda5d
zhekichaan <eugene.chernitskiy@gmail.com>
2022-12-25 08:13:47 -0500

    fix addMyProducts

--- controllers/myProducts/addMyProducts.js
+++ controllers/myProducts/addMyProducts.js
@@ -12,6 +12,7 @@ const addMyProducts = async (req, res) => {
     productInfo: { $elemMatch: { productName } },
   });
   if (product) {
+    console.log(1);
     const index = product.productInfo.findIndex(
       (product) => product.productName === productName
     );
@@ -48,8 +49,9 @@ const addMyProducts = async (req, res) => {
   }
 
   if (await MyProducts.findOne({ date, owner: _id })) {
+    console.log(2);
     const productUpdate = await MyProducts.findOneAndUpdate(
-      { date },
+      { date, owner: _id },
       {
         $push: {
           productInfo: { productCalories, productName, productWeight },
@@ -61,6 +63,7 @@ const addMyProducts = async (req, res) => {
       .status(201)
       .json({ success: "success", code: 201, productUpdate });
   }
+  console.log(3);
 
   const productAdd = await MyProducts.create({
     date,
