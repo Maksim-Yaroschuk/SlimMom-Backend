22d17dd10bafa3f840226d0160477c45b1580fc2
zhekichaan <eugene.chernitskiy@gmail.com>
2022-12-28 09:36:40 -0500

    fix deleteMyProducts

--- controllers/myProducts/deleteMyProducts.js
+++ controllers/myProducts/deleteMyProducts.js
@@ -4,6 +4,8 @@ const { NotFound } = require("http-errors");
 const deleteMyProducts = async (req, res) => {
   const { productId } = req.params;
   const { date } = req.body;
+  const { _id } = req.user;
+
 
   const product = await MyProducts.findOneAndUpdate(
     { date, productInfo: { $elemMatch: { _id: productId } } },
@@ -24,10 +26,15 @@ const deleteMyProducts = async (req, res) => {
     NotFound(`Product with id = ${productId} not found`);
   }
 
+  const newProduct = await MyProducts.findOne({
+    date,
+    owner: _id
+  })
+
   return res.status(200).json({
     status: "success",
     code: 200,
-    data: { product },
+    newProduct,
   });
 };
 
